744481a279d2606492c3e1a482874dd362de04b7
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-18 12:40:14 +0545

    Client: add sign in page

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -1,6 +1,51 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link";
+
 const SignIn = () => {
     return (
-        <div>This is SignIn Page..</div>
+        <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-500 to-purple-500 p-4">
+            <Card className="w-full max-w-sm shadow-2xl">
+                <CardHeader className="text-center">
+                    <CardTitle>
+                        Sign In to your account
+                    </CardTitle>
+                    <CardDescription>
+                        Enter your credentials to Sign In
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-2">
+                        <Label>Email</Label>
+                        <Input placeholder="abc@example.com" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <Label>Password</Label>
+                        <Input type="password" placeholder="Enter your password" />
+                    </div>
+                </CardContent>
+
+                <CardFooter className="flex flex-col gap-3">
+                    <Button className="w-full bg-blue-500 hover:bg-blue-600">
+                        Sign In
+                    </Button>
+
+                    <p className="text-sm text-center text-gray-600">
+                        Don't have an account?{" "}
+                        <Link
+                            href="/register"
+                            className="text-blue-600 font-medium hover:underline"
+                        >
+                            Register
+                        </Link>
+                    </p>
+                </CardFooter>
+            </Card>
+        </div>
     );
 };
 
